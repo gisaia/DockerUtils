718dc8120d53dcbe1401a6ea9f1bb37ec2a4ceab
alainbodiguel <alain.bodiguel@gisaia.com>
2021-11-18 12:10:46 +0100

    New image for java 17

--- arlas-openjdk/release.sh
+++ arlas-openjdk/release.sh
@@ -6,3 +6,9 @@ docker push gisaia/arlas-openjdk:8-jre-alpine
 
 docker build -f Dockerfile --tag gisaia/arlas-openjdk:8-bullseye .
 docker push gisaia/arlas-openjdk:8-bullseye
+
+docker build -f Dockerfile-alpine-17 --tag gisaia/arlas-openjdk:17-alpine .
+docker push gisaia/arlas-openjdk:17-alpine
+
+docker build -f Dockerfile-17 --tag gisaia/arlas-openjdk:17 .
+docker push gisaia/arlas-openjdk:17
