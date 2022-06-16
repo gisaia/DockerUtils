f4b68b6981c106c9a5f006af0b2f9d5b76ec318a
alainbodiguel <alain.bodiguel@gisaia.com>
2022-06-16 09:13:41 +0200

    New distroless based java image

--- arlas-openjdk/release.sh
+++ arlas-openjdk/release.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
-docker build -f Dockerfile-alpine --tag gisaia/arlas-openjdk:8-jre-alpine .
-docker push gisaia/arlas-openjdk:8-jre-alpine
-
-docker build -f Dockerfile --tag gisaia/arlas-openjdk:8-bullseye .
-docker push gisaia/arlas-openjdk:8-bullseye
-
-docker build -f Dockerfile-alpine-17 --tag gisaia/arlas-openjdk:17-alpine .
-docker push gisaia/arlas-openjdk:17-alpine
-
-docker build -f Dockerfile-17 --tag gisaia/arlas-openjdk:17 .
-docker push gisaia/arlas-openjdk:17
+docker build -f Dockerfile-distroless-17 --tag gisaia/arlas-openjdk:17-distroless .
+docker push gisaia/arlas-openjdk:17-distroless
