bfc6c8407432b6d64cdb0af9cc0b76f1180760a9
alainbodiguel <alain.bodiguel@gisaia.com>
2024-03-25 09:51:15 +0100

    Add a version nubmer to arlas-openjdk image

--- arlas-openjdk/release.sh
+++ arlas-openjdk/release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -o errexit -o pipefail
 
+VERSION=`date '+%Y%m%d%H%M%S'`
 docker pull  gcr.io/distroless/java17-debian12
-docker build --no-cache -f Dockerfile-distroless-17 --tag gisaia/arlas-openjdk:17-distroless .
-docker push gisaia/arlas-openjdk:17-distroless
+docker build --no-cache -f Dockerfile-distroless-17 --tag gisaia/arlas-openjdk-17-distroless:$VERSION .
+docker push gisaia/arlas-openjdk-17-distroless:$VERSION
+
+docker tag gisaia/arlas-openjdk-17-distroless:$VERSION gisaia/arlas-openjdk-17-distroless:latest
+docker push gisaia/arlas-openjdk-17-distroless:latest
+
+echo "gisaia/arlas-openjdk-17-distroless:$VERSION released"
