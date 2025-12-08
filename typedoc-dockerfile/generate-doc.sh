9206d18c06bd9c52e7df8eaa1175f9fe3dca59de
Sebastien Bousquet <sebastien.bousquet@gisaia.com>
2025-12-08 15:42:24 +0100

    typedoc: upgrade typedoc & typedoc-markdown & release 0.0.11

--- typedoc-dockerfile/generate-doc.sh
+++ typedoc-dockerfile/generate-doc.sh
@@ -9,7 +9,7 @@ mkdir typedoc_docs
 
 ## INSTALL DEPENDENCIES OF THE MOUNTED FOLDER + INSTALL TYPEDOC AND MARKDOWN PLUGIN
 npm install
-npm add typedoc@0.27.4 typedoc-plugin-markdown@4.3.2
+npm add typedoc@0.27.9 typedoc-plugin-markdown@4.4.2
 
 ## GENERATE THE DOCUMENTATTION
 node_modules/.bin/typedoc --plugin typedoc-plugin-markdown --excludePrivate --out ./typedoc_docs/ $(echo "$@"| tr " " "\n")
