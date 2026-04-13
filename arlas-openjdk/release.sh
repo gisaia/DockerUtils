53d5f9ff4f6d68230a148c06176ceaa2b6ed45ce
Sylvain Gaudan <sylvain.gaudan@gisaia.com>
2026-04-13 12:28:01 +0200

    upgrade distroless to debian 13

--- arlas-openjdk/release.sh
+++ arlas-openjdk/release.sh
@@ -2,7 +2,6 @@
 set -o errexit -o pipefail
 
 VERSION=`date '+%Y%m%d%H%M%S'`
-docker pull  gcr.io/distroless/java17-debian12
 docker build --no-cache -f Dockerfile-distroless-17 --tag gisaia/arlas-openjdk-17-distroless:$VERSION .
 docker push gisaia/arlas-openjdk-17-distroless:$VERSION
 
